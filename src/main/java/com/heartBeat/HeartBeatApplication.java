72b58cf71e8375549098359252bb263a5cdf3f57
Deepak Singh <deepsinghkumar01@gmail.com>
2024-07-25 18:21:03 +0530

    Add Profile record definition and save functionality
    - Define Profile as a record for concise data representation.
    - Implement saving a sample profile using ProfileRepository.
    - Verify functionality by successfully saving and retrieving profiles.

--- src/main/java/com/heartBeat/HeartBeatApplication.java
+++ src/main/java/com/heartBeat/HeartBeatApplication.java
@@ -1,13 +1,38 @@
 package com.heartBeat;
 
+import com.heartBeat.profiles.Gender;
+import com.heartBeat.profiles.Profile;
+import com.heartBeat.profiles.ProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HeartBeatApplication {
+public class HeartBeatApplication  implements CommandLineRunner {
+
+	@Autowired
+	private ProfileRepository profileRepository;
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HeartBeatApplication.class, args);
 	}
 
+	public  void run(String... args){
+		//System.out.println("Running");
+		Profile profile = new Profile(
+				"1",
+				"Deepak",
+				"Singh",
+				23,
+				"Indian",
+				Gender.MALE,
+				"Software Developer",
+				"abc.jpg",
+				"INTP"
+		);
+		profileRepository.save(profile);
+		profileRepository.findAll().forEach(System.out::println);
+	}
 }
---
+++ src/main/java/com/heartBeat/profiles/Gender.java
@@ -0,0 +1,7 @@
+package com.heartBeat.profiles;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    NON_BINARY
+}
