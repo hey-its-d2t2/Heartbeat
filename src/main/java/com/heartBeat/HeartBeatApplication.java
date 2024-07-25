fa070b905ec598223368fa1cbbcbd5918e93b91f
Deepak Singh <deepsinghkumar01@gmail.com>
2024-07-25 17:41:02 +0530

    Project initialized.

---
+++ src/main/java/com/heartBeat/HeartBeatApplication.java
@@ -0,0 +1,13 @@
+package com.heartBeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartBeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeartBeatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heartBeat/HeartBeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heartBeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeartBeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
